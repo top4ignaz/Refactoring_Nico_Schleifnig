a91d10c345e8d9cbddaff1bd35485357f0b9eafe
Nico Schleifnig <nico.schleifnig@gmx.de>
2019-05-07 20:27:14 +0200

    Adding htmlStatement() Method

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -26,15 +26,25 @@ public class Customer {
 
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
-			// determine amounts for each line
-			// add frequent renter points
-			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+		return result;
+	}
+
+	public String htmlStatement() {
+		Enumeration enum_rentals = rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></ H1><P>\n";
+		while (enum_rentals.hasMoreElements()) {
+			Rental each = (Rental) enum_rentals.nextElement();
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
 		return result;
 	}
 
